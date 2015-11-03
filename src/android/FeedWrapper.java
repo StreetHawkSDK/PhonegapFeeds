dcf842e47a2e2209db073c3520a2194713aed337
Anurag Kondeya <anurag.kondeya@gmail.com>
2015-11-03 17:24:00 +1100

    Fixed feeds API

--- src/android/FeedWrapper.java
+++ src/android/FeedWrapper.java
@@ -9,22 +9,59 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import com.streethawk.library.feeds.ISHFeedItemObserver;
 import com.streethawk.library.feeds.SHFeedItem;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import com.streethawk.library.core.StreetHawk;
 
 /**
  * FeedWrapper receives feed item from server and passes the same to application.
  */
-public class FeedWrapper implements ISHFeedItemObserver {
+public class FeedWrapper extends BroadcastReceiver implements ISHFeedItemObserver {
 	public static FeedWrapper mInstance = null;
 	private CallbackContext mFeedItemCallback=null;
+	private static CallbackContext mNotifyNewFeedCallback=null;
 	private static Context mContext;
 	private FeedWrapper(){}
 
+	@Override
+	public void onReceive(Context context, Intent intent){
+		String action = intent.getAction();
+		if (action.equals("com.streethawk.intent.action.newfeed")) {
+			 String installId = intent.getStringExtra("installid");
+	            if(null==installId)
+	                return;
+	            if (!(installId.equals(StreetHawk.INSTANCE.getInstallId(context)))) {
+	            	return;
+	            }
+			if(null!=mNotifyNewFeedCallback){
+				try{
+				JSONObject feedValue = new JSONObject(); 
+				feedValue.put("NEW_FEED",1); //TODO correct feed number here
+				PluginResult result = new PluginResult(PluginResult.Status.OK,feedValue);
+				result.setKeepCallback(true);
+				this.mFeedItemCallback.sendPluginResult(result);
+				}catch(JSONException e){
+					return;
+				}
+			}
+		}
+	}
+
+	public void notifyNewFeedCallback(Context context,CallbackContext cb){
+		if(null!=context){
+			mContext = context;
+			mNotifyNewFeedCallback = cb;
+		}
+	}
+	
+	
 	public static FeedWrapper getInstance(){
 		if(null==mInstance)
 			mInstance = new FeedWrapper();
 		return mInstance;
 	}
-	
+
 	public void registerFeed(Context context,CallbackContext cb){
 		if(null!=context){
 			mContext = context;
